b676257d4408ed4a45468c2d8c9e7a25b6123f8a
EricSweeney <ejs363@drexel.edu>
2022-04-14 19:52:36 -0400

    Initial commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,21 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
 
 //Replace this value with your name
-const yourName = "Your Name"
-document.querySelector("#name").innerHTML = yourName;
+//const yourName = "Eric Sweeney"
+document.querySelector("#name").innerHTML = "Eric Sweeney"();
 
 //GreenSock Timeline
+
+const mainTL = gsap.timeline();
+
+mainTL.from(".red", {duration: 2, opacity:0}, "box")
+.from(".blue", {duration: 2, opacity:0}, "box")
+
+.from(".red", {duration: 1, x:0, ease:"bounce.out"}, "box1")
+.from(".blue", {duration:1, x:350, ease:"bounce.out"}, "box1")
+
+.to(".red", {duration: 2, y:0, rotation: 360}, "box2")
+.to(".blue", {duration: 2, y:300, rotation:360}, "box2")
+
+
+;
